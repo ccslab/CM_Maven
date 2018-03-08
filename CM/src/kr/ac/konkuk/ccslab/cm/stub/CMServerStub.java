f3251d2cf376d67e5c91a263508f24811e6ad34d
Mingyu Lim <mlim@konkuk.ac.kr>
2018-03-08 23:38:43 +0900

    added javadoc comments in the CMServerStub class.
    modified:   CM/src/kr/ac/konkuk/ccslab/cm/stub/CMServerStub.java
    	:	added comments for the terminateCM(), requestServerReg(), requestServerDereg(), connectToServer(), disconnectFromServer(), setAttachDownloadScheme() methods.

--- CM/src/kr/ac/konkuk/ccslab/cm/stub/CMServerStub.java
+++ CM/src/kr/ac/konkuk/ccslab/cm/stub/CMServerStub.java
@@ -86,6 +86,13 @@ public class CMServerStub extends CMStub {
 		return true;
 	}
 	
+	/**
+	 * Terminates the server CM.
+	 * <br>A server application calls this method when it does not need to use CM any more. 
+	 * The server releases all the resources that are used by CM.
+	 * 
+	 * @see CMServerStub#startCM()
+	 */
 	public void terminateCM()
 	{
 		super.terminateCM();
@@ -94,6 +101,43 @@ public class CMServerStub extends CMStub {
 			System.out.println("CMServerStub.terminateCM(), succeeded.");
 	}
 	
+	/**
+	 * Registers an additional server to the default server.
+	 * 
+	 * <p> When an additional server starts, it automatically connects to the default server. 
+	 * The additional server then needs to request for registration to the default server in order to 
+	 * participate in current CM network.
+	 * <br> Only an additional server should call the requestServerReg method with a desired server name. 
+	 * Because the default server has the reserved name, "SERVER", the additional server must specify 
+	 * a different name as the parameter of this method.
+	 * <br> In order for a requesting server to check the result of the registration request, 
+	 * the server can catch the RES_SERVER_REG event of the CMMultiServerEvent class in its event handler routine. 
+	 * The event fields of this event are described below.
+	 * 
+	 * <table border=1>
+	 * <tr>
+	 * <td bgcolor="lightgrey">Event type</td><td>CMInfo.CM_MULTI_SERVER_EVENT</td>
+	 * </tr>
+	 * <tr>
+	 * <td bgcolor="lightgrey">Event ID</td><td>CMMultiServerEvent.RES_SERVER_REG</td>
+	 * </tr>
+	 * <tr bgcolor="lightgrey">
+	 * <td>Event field</td><td>Field data type</td><td>Field definition</td><td>Get method</td>
+	 * </tr>
+	 * <tr>
+	 * <td>server name</td><td>String</td><td>the requester server name</td><td>getServerName()</td>
+	 * </tr>
+	 * <tr>
+	 * <td>return code</td><td>int</td>
+	 * <td>result code of the registration request
+	 * <br>1: succeeded<br>0: failed</td>
+	 * <td>getReturnCode()</td>
+	 * </tr>
+	 * </table> 
+	 * 
+	 * @param server - the server name
+	 * @see CMServerStub#requestServerDereg()
+	 */
 	public void requestServerReg(String server)
 	{
 		CMConfigurationInfo confInfo = m_cmInfo.getConfigurationInfo();
@@ -131,6 +175,41 @@ public class CMServerStub extends CMStub {
 		return;
 	}
 	
+	/**
+	 * Deregisters an additional server from the default server.
+	 * 
+	 * <p> If an additional server leaves current CM network, it can request to deregister from the default server.
+	 * Although it leaves the CM network, the additional server still maintains the connection with the default server. 
+	 * If required, this connection can also be managed by the {@link CMServerStub#connectToServer()} and 
+	 * the {@link CMServerStub#disconnectFromServer()} methods.
+	 * 
+	 * <br> In order for a requesting server to check the result of the deregistration request, 
+	 * the server can catch the RES_SERVER_DEREG event of the CMMultiServerEvent class in its event handler routine. 
+	 * The event fields of this event are described below.
+	 * 
+	 * <table border=1>
+	 * <tr>
+	 * <td bgcolor="lightgrey">Event type</td><td>CMInfo.CM_MULTI_SERVER_EVENT</td>
+	 * </tr>
+	 * <tr>
+	 * <td bgcolor="lightgrey">Event ID</td><td>CMMultiServerEvent.RES_SERVER_DEREG</td>
+	 * </tr>
+	 * <tr bgcolor="lightgrey">
+	 * <td>Event field</td><td>Field data type</td><td>Field definition</td><td>Get method</td>
+	 * </tr>
+	 * <tr>
+	 * <td>server name</td><td>String</td><td>the requester server name</td><td>getServerName()</td>
+	 * </tr>
+	 * <tr>
+	 * <td>return code</td><td>int</td>
+	 * <td>result code of the deregistration request
+	 * <br>1: succeeded<br>0: failed</td>
+	 * <td>getReturnCode()</td>
+	 * </tr>
+	 * </table> 
+	 * 
+	 * @see CMServerStub#requestServerReg(String)
+	 */
 	public void requestServerDereg()
 	{
 		CMInteractionInfo interInfo = m_cmInfo.getInteractionInfo();
@@ -160,7 +239,15 @@ public class CMServerStub extends CMStub {
 		return;
 	}
 	
-	// connect to the default server
+	/**
+	 * Connects to the default server.
+	 * 
+	 * <p> An additional server can call this method to establish a connection to 
+	 * the default server.
+	 * 
+	 * @return true if the connection is successfully established, or false otherwise.
+	 * @see CMServerStub#disconnectFromServer()
+	 */
 	public boolean connectToServer()
 	{
 		boolean result = false;
@@ -172,8 +259,16 @@ public class CMServerStub extends CMStub {
 		result = CMInteractionManager.connectDefaultServer(m_cmInfo);
 		return result;
 	}
-	
-	// disconnect from the default server
+
+	/**
+	 * Disconnects from the default server.
+	 * 
+	 * <p> An additional server can call this method to disconnect the connection from 
+	 * the default server.
+	 * 
+	 * @return true if the connection is successfully disconnected, or false otherwise.
+	 * @see CMServerStub#connectToServer()
+	 */
 	public boolean disconnectFromServer()
 	{
 		boolean result = false;
@@ -186,6 +281,53 @@ public class CMServerStub extends CMStub {
 		return result;
 	}
 	
+	/**
+	 * Sets the download scheme for attached images of SNS content.
+	 *
+	 * <p> The detailed information about the attachment download scheme can be found 
+	 * in the following reference: 
+	 * <br> <i>Mingyu Lim, "Multi-level Content Transmission Mechanism for Intelligent Quality of Service 
+	 * in Social Networking Services," The Transactions on the Korean Institute of Electrical Engineers, 
+	 * Vol. 65, No. 8, August 2016, pp.1407-1417.</i>
+	 * 
+	 * @param strUserName - the target user name
+	 * <br> The attachment download scheme is applied to 'strUserName'. If the value is null, 
+	 * the download scheme is applied to all users.
+	 * @param nScheme - the download scheme
+	 * <br> The possible value is CMInfo.SNS_ATTACH_FULL(or 0), CMInfo.SNS_ATTACH_PARTIAL(or 1), 
+	 * CMInfo.SNS_ATTACH_PREFETCH(or 2) and CMInfo.SNS_ATTACH_NONE(or 3).
+	 * <table border=1>
+	 * <tr bgcolor=lightgrey>
+	 * <td>download scheme</td><td>description</td>
+	 * </tr>
+	 * <tr>
+	 * <td>CMInfo.SNS_ATTACH_FULL</td>
+	 * <td>
+	 * The CM server sends images with the original quality to the client.
+	 * </td>
+	 * </tr>
+	 * <tr>
+	 * <td>CMInfo.SNS_ATTACH_PARTIAL</td>
+	 * <td>
+	 * The server sends thumbnail images instead of the original images.
+	 * </td>
+	 * </tr>
+	 * <tr>
+	 * <td>CMInfo.SNS_ATTACH_PREFETCH</td>
+	 * <td>
+	 * The server sends thumbnail images to the client, and sends also original 
+	 * images that the client is interested in.
+	 * </td>
+	 * </tr>
+	 * <tr>
+	 * <td>CMInfo.SNS_ATTACH_NONE</td>
+	 * <td>
+	 * The server sends only text links to images.
+	 * </td>
+	 * </tr>
+	 * </table>
+	 * @see CMClientStub#requestSNSContent(String, int)
+	 */
 	// change the download scheme for the attachment of SNS content
 	public void setAttachDownloadScheme(String strUserName, int nScheme)
 	{
