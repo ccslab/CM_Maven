840a84542412cecc02d10f2740b3c330f7d006b4
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-15 11:45:31 +0900

    added methods to the CMMqttEventFixedHeader class.
    added marshallFixedHeader() and unmarshallFixedHeader() methods.
    
    현재 브랜치 pubsub
    수정함:        CM/src/kr/ac/konkuk/ccslab/cm/event/CMMqttEventFixedHeader.java

--- CM/src/kr/ac/konkuk/ccslab/cm/event/CMMqttEventFixedHeader.java
+++ CM/src/kr/ac/konkuk/ccslab/cm/event/CMMqttEventFixedHeader.java
@@ -1,5 +1,11 @@
 package kr.ac.konkuk.ccslab.cm.event;
 
+import java.nio.ByteBuffer;
+
+import com.mysql.jdbc.Buffer;
+
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+
 /**
  * This class represents CM events that belong to the fixed header of 
  * MQTT control packets.
@@ -8,9 +14,9 @@ package kr.ac.konkuk.ccslab.cm.event;
  */
 public abstract class CMMqttEventFixedHeader extends CMMqttEvent {
 
-	private byte m_packetType; // 4 bits, 1~14
-	private byte m_flag; //	4 bits
-	private int m_nRemainingLength;	// 1~4 bytes
+	protected byte m_packetType; // 4 bits, 1~14
+	protected byte m_flag; //	4 bits
+	protected int m_nRemainingLength;	// 1~4 bytes
 	
 	public CMMqttEventFixedHeader()
 	{
@@ -80,14 +86,90 @@ public abstract class CMMqttEventFixedHeader extends CMMqttEvent {
 	@Override
 	protected void marshallFixedHeader() {
 		// TODO Auto-generated method stub
-
-		// from here
+		
+		// encoding byte 1: packet type(4 bits) + flag (4 bits)
+		byte byteTypeFlag = 0;
+		byteTypeFlag = (byte)((byteTypeFlag | m_packetType) << 4);
+		byteTypeFlag = (byte)(byteTypeFlag | m_flag);
+		// print current value and the encoded byte
+		if(CMInfo._CM_DEBUG)
+		{
+			System.out.println("CMMqttEventFixedHeader.marshallFixedHeader(): ");
+			System.out.println("packet type: "+m_packetType+", flag: "+m_flag);
+			System.out.println("encoded byte: "+String.format("%08x", byteTypeFlag));
+		}
+		// put the encoded type and flag to the ByteBuffer
+		m_bytes.put(byteTypeFlag);
+		
+		// encoding byte 2: remaining length (1 ~ 4 bytes)
+		// print current value
+		if(CMInfo._CM_DEBUG)
+		{
+			System.out.println("remaining length: "+m_nRemainingLength);
+		}
+		// The encoding rule conforms to MQTT v3.1.1 (page 19)
+		int X = m_nRemainingLength;
+		do {
+			byte encodedByte = (byte)(X % 128);
+			X = X / 128;
+			// if there are more data to encode, set the top bit of this byte
+			if( X > 0 )
+				encodedByte = (byte)(encodedByte | 128);
+			// put the encoded byte to the ByteBuffer
+			m_bytes.put(encodedByte);
+			// print the encoded byte
+			if(CMInfo._CM_DEBUG)
+			{
+				System.out.println("encoded reamining length byte: "+
+						String.format("%08x", encodedByte));
+			}
+		} while( X > 0 );
+		
+		return;
 	}
 
 	@Override
-	protected void unmarshallFixedHeader() {
+	protected void unmarshallFixedHeader(ByteBuffer buf) {
 		// TODO Auto-generated method stub
 
+		// decoding byte 1: packet type (4 bits) + flag (4 bits)
+		byte byteTypeFlag = buf.get();
+		m_packetType &= 0x00; // initialize the variable
+		m_packetType = (byte)((byteTypeFlag | 0xf0) >> 4);
+		m_flag &= 0x00;	// initizliae the variable
+		m_flag = (byte)(byteTypeFlag | 0x0f);
+		// print current byte value and the decoded packet type and flag
+		if(CMInfo._CM_DEBUG)
+		{
+			System.out.println("CMMqttEventFixedHeader.unmarshallFixedHeader(): ");
+			System.out.println("byte 1: "+String.format("%08x",  byteTypeFlag));
+			System.out.println("packet type: "+m_packetType+", flag: "+m_flag);
+		}
+		
+		// decoding byte 2: remaining length (1 ~ 4 bytes)
+		// The decoding rule conforms to MQTT v3.1.1 (page 19)
+		int nMultiplier = 1;
+		byte encodedByte = 0;
+		m_nRemainingLength = 0;
+		do {
+			encodedByte = buf.get();
+			// print the encoded byte
+			if(CMInfo._CM_DEBUG)
+				System.out.println("remaining length byte: "+String.format("%08x", encodedByte));
+			m_nRemainingLength += (encodedByte & 127)*nMultiplier;
+			nMultiplier *= 128;
+			if(nMultiplier > 128*128*128)
+			{
+				System.err.println("malformed remaining length!");
+				return;
+			}
+		} while( (encodedByte & 128) != 0 );
+		
+		// print the decoded remaining length
+		if(CMInfo._CM_DEBUG)
+			System.out.println("remaining length: "+m_nRemainingLength);
+		
+		return;
 	}
 
 	/*
@@ -95,8 +177,8 @@ public abstract class CMMqttEventFixedHeader extends CMMqttEvent {
 	protected abstract int getVarHeaderByteNum();
 	protected abstract int getPayloadByteNum();
 	protected abstract void marshallVarHeader();
-	protected abstract void unmarshallVarHeader();
+	protected abstract void unmarshallVarHeader(ByteBuffer buf);
 	protected abstract void marshallPayload();
-	protected abstract void unmarshallPayload();
+	protected abstract void unmarshallPayload(ByteBuffer buf);
 	*/
 }
