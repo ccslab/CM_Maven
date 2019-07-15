4a3b79b4b7dc9857a133f39164aa3dbc523462e9
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-15 13:52:05 +0900

    modified javadoc comments of the stub and event packages.
    현재 브랜치 pubsub
    수정함:        CM/src/kr/ac/konkuk/ccslab/cm/event/package-info.java
    수정함:        CM/src/kr/ac/konkuk/ccslab/cm/stub/package-info.java

--- CM/src/kr/ac/konkuk/ccslab/cm/event/package-info.java
+++ CM/src/kr/ac/konkuk/ccslab/cm/event/package-info.java
@@ -1,8 +1,6 @@
 /**
- * 
- */
-/**
- * @author mlim
+ * The event package contains CM events that CM entities send and receive to communicate with each other.
+ * @author CCSLab, Konkuk University
  *
  */
 package kr.ac.konkuk.ccslab.cm.event;
--- CM/src/kr/ac/konkuk/ccslab/cm/stub/package-info.java
+++ CM/src/kr/ac/konkuk/ccslab/cm/stub/package-info.java
@@ -1,8 +1,6 @@
 /**
- * Contains stub classes with which an application can interact with CM. 
+ * The stub package contains stub classes with which an application can interact with CM. 
  * The application can access CM APIs and other internal CM objects through the stub object.
- */
-/**
  * @author CCSLab, Konkuk University
  *
  */
