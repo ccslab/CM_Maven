3abc249a393cdf5bf3bf1432ac37fe3ba4326229
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-16 14:13:47 +0900

    created CMMqttEventTest.java file.
    This executable file tests marshalling/unmarshalling of mqtt event classes.
    
    현재 브랜치 pubsub
    새 파일:       CM/src/CMMqttEventTest.java

---
+++ CM/src/CMMqttEventTest.java
@@ -0,0 +1,12 @@
+import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventCONNECT;
+
+public class CMMqttEventTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CMMqttEventCONNECT mqttCONNECTEvent = new CMMqttEventCONNECT();
+		System.out.println(mqttCONNECTEvent.toString());
+	}
+
+}
