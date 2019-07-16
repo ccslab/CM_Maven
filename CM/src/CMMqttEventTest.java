5189c3fddb7fb9ec8c9b8f64eedca8e05e339a17
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-16 21:50:01 +0900

    added the test of CMMqttEventCONNACK to the CMMqttEventTest class.
    현재 브랜치 pubsub
    수정함:        CM/src/CMMqttEventTest.java

--- CM/src/CMMqttEventTest.java
+++ CM/src/CMMqttEventTest.java
@@ -1,12 +1,20 @@
 import java.nio.ByteBuffer;
 
+import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventCONNACK;
 import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventCONNECT;
 
 public class CMMqttEventTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CMMqttEventTest tester = new CMMqttEventTest();
+		
+		tester.testCONNECT();
+		tester.testCONNACK();
+	}
 
+	private void testCONNECT()
+	{
 		System.out.println("===================");
 		CMMqttEventCONNECT mqttCONNECTEvent = new CMMqttEventCONNECT();
 		mqttCONNECTEvent.setUserNameFlag(true);
@@ -27,7 +35,20 @@ public class CMMqttEventTest {
 		CMMqttEventCONNECT mqttCONNECTEvent2 = new CMMqttEventCONNECT(buf);
 		
 		System.out.println("------------------- after marshalling/unmarshalling the event");
-		System.out.println(mqttCONNECTEvent2.toString());
+		System.out.println(mqttCONNECTEvent2.toString());		
 	}
-
+	
+	private void testCONNACK()
+	{
+		System.out.println("===================");
+		CMMqttEventCONNACK mqttConnack = new CMMqttEventCONNACK();
+		mqttConnack.setVarHeader(true, (byte)5);
+		System.out.println("------------------- after setting member variables");
+		System.out.println(mqttConnack.toString());
+		
+		ByteBuffer buf = mqttConnack.marshall();
+		CMMqttEventCONNACK mqttConnack2 = new CMMqttEventCONNACK(buf);
+		System.out.println("------------------- after marshalling/unmarshalling the event");
+		System.out.println(mqttConnack2.toString());
+	}
 }
