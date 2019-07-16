27f2d79690d5d473570a8fd0c7eab6911c5625eb
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-16 14:18:05 +0900

    added the test of marshalling/unmarshalling of CMMqttEventCONNECT.
    현재 브랜치 pubsub
    수정함:        CM/src/CMMqttEventTest.java

--- CM/src/CMMqttEventTest.java
+++ CM/src/CMMqttEventTest.java
@@ -1,3 +1,5 @@
+import java.nio.ByteBuffer;
+
 import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventCONNECT;
 
 public class CMMqttEventTest {
@@ -7,6 +9,26 @@ public class CMMqttEventTest {
 
 		CMMqttEventCONNECT mqttCONNECTEvent = new CMMqttEventCONNECT();
 		System.out.println(mqttCONNECTEvent.toString());
+		
+		mqttCONNECTEvent.setUserNameFlag(true);
+		mqttCONNECTEvent.setWillQoS((byte)2);
+		mqttCONNECTEvent.setWillFlag(true);
+		mqttCONNECTEvent.setKeepAlive(100);
+		
+		mqttCONNECTEvent.setClientID("mqtt-test-client");
+		mqttCONNECTEvent.setWillTopic("/CM/mqtt");
+		mqttCONNECTEvent.setWillMessage("mqtt-connect-test-message");
+		mqttCONNECTEvent.setUserName("ccslab");
+		mqttCONNECTEvent.setPassword("ccslab");
+		
+		System.out.println("----- after setting CONNECT packet");
+		System.out.println(mqttCONNECTEvent.toString());
+		
+		ByteBuffer buf = mqttCONNECTEvent.marshall();
+		CMMqttEventCONNECT mqttCONNECTEvent2 = new CMMqttEventCONNECT(buf);
+		
+		System.out.println("----- after marshalling/unmarshalling the event");
+		System.out.println(mqttCONNECTEvent2.toString());
 	}
 
 }
