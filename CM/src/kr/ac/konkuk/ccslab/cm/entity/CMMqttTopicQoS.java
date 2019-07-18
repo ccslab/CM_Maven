b1b8edfc9a8b7d4ef9123983ee9411036590101f
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-18 11:07:41 +0900

    created CMMqttTopicQoS class.
    added member variables and methods.
    This class is used in the subscription process of the MQTT protocol.
    
    현재 브랜치 pubsub
    새 파일:       CM/src/kr/ac/konkuk/ccslab/cm/entity/CMMqttTopicQoS.java

---
+++ CM/src/kr/ac/konkuk/ccslab/cm/entity/CMMqttTopicQoS.java
@@ -0,0 +1,67 @@
+package kr.ac.konkuk.ccslab.cm.entity;
+
+/**
+ * The CMMqttTopicQoS class represents a pair of a topic fileter and a requested QoS, 
+ * and it is used in the topic subscription process of the MQTT protocol.
+ * 
+ * @author CCSLab, Konkuk University
+ *
+ */
+public class CMMqttTopicQoS {
+
+	private String m_strTopic;
+	private byte m_qos;
+	
+	public CMMqttTopicQoS()
+	{
+		m_strTopic = "";
+		m_qos = 0;
+	}
+	
+	public CMMqttTopicQoS(String strTopic, byte qos)
+	{
+		m_strTopic = strTopic;
+		m_qos = qos;
+	}
+	
+	public void setTopic(String strTopic)
+	{
+		m_strTopic = strTopic;
+	}
+	
+	public String getTopic()
+	{
+		return m_strTopic;
+	}
+	
+	public void setQoS(byte qos)
+	{
+		m_qos = qos;
+	}
+	
+	public byte getQoS()
+	{
+		return m_qos;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj == null) return false;
+		if(!this.getClass().equals(obj.getClass())) return false;
+		if(this == obj) return true;
+		
+		CMMqttTopicQoS topicQoS = (CMMqttTopicQoS) obj;
+		if(m_strTopic.contentEquals(topicQoS.getTopic()))
+			return true;
+		else
+			return false;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String strTopicQoS = "CMMqttTopicQoS{ \"topic\": \""+m_strTopic+"\", \"qos\": "+m_qos+" }";
+		return strTopicQoS;
+	}
+}
