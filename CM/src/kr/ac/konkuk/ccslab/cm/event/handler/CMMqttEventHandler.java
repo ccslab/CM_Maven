7d98a2e217c73e436ce6f93cf5f25af2d433caeb
Mingyu Lim <mlim@konkuk.ac.kr>
2019-07-23 18:00:05 +0900

    (ongoing) added empty private methods in CMMqttEventHandler.
    현재 브랜치 pubsub
    수정함:        CM/src/kr/ac/konkuk/ccslab/cm/event/handler/CMMqttEventHandler.java

--- CM/src/kr/ac/konkuk/ccslab/cm/event/handler/CMMqttEventHandler.java
+++ CM/src/kr/ac/konkuk/ccslab/cm/event/handler/CMMqttEventHandler.java
@@ -1,6 +1,9 @@
 package kr.ac.konkuk.ccslab.cm.event.handler;
 
 import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEvent;
+import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventCONNECT;
+import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEventPUBLISH;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 
 /**
@@ -23,4 +26,84 @@ public class CMMqttEventHandler extends CMEventHandler {
 		return false;
 	}
 
+	private boolean processCONNECT(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean validateCONNECT(CMMqttEventCONNECT conEvent)
+	{
+		return false;
+	}
+	
+	private boolean processCONNACK(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPUBLISH(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean sendPUBACK(CMMqttEventPUBLISH pubEvent)
+	{
+		return false;
+	}
+	
+	private boolean sendPUBREC(CMMqttEventPUBLISH pubEvent)
+	{
+		return false;
+	}
+	
+	private boolean processPUBACK(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPUBREC(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPUBREL(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPUBCOMP(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processSUBSCRIBE(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processSUBACK(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processUNSUBSCRIBE(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPINGREQ(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processPINGRESP(CMMqttEvent event)
+	{
+		return false;
+	}
+	
+	private boolean processDISCONNECT(CMMqttEvent event)
+	{
+		return false;
+	}
+	
 }
