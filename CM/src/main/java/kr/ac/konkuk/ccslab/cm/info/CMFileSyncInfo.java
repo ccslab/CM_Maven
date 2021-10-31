701ec14138712fca348ba5c6ba7fa3edea47d6ce
Mingyu Lim <mlim@konkuk.ac.kr>
2021-10-31 20:15:50 +0900

    added createPathList() in CMFileSyncManager.

--- CM/src/main/java/kr/ac/konkuk/ccslab/cm/info/CMFileSyncInfo.java
+++ CM/src/main/java/kr/ac/konkuk/ccslab/cm/info/CMFileSyncInfo.java
@@ -1,12 +1,17 @@
 package kr.ac.konkuk.ccslab.cm.info;
 
+import java.nio.file.Path;
+import java.util.List;
+
 public class CMFileSyncInfo {
 
     public static final String SYNC_HOME = "FileSyncHome";
     private boolean syncInProgress;
+    private List<Path> pathList;
 
     public CMFileSyncInfo() {
         syncInProgress = false;
+        pathList = null;
     }
 
     public boolean isSyncInProgress() {
@@ -15,5 +20,13 @@ public class CMFileSyncInfo {
 
     public void setSyncInProgress(boolean syncInProgress) {
         this.syncInProgress = syncInProgress;
+    }
+
+    public List<Path> getPathList() {
+        return pathList;
+    }
+
+    public void setPathList(List<Path> pathList) {
+        this.pathList = pathList;
     }
 }
--- CM/src/main/java/kr/ac/konkuk/ccslab/cm/manager/CMFileSyncManager.java
+++ CM/src/main/java/kr/ac/konkuk/ccslab/cm/manager/CMFileSyncManager.java
@@ -7,6 +7,8 @@ import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CMFileSyncManager extends CMServiceManager {
 
@@ -29,6 +31,7 @@ public class CMFileSyncManager extends CMServiceManager {
 
     public boolean startFileSync() {
 
+        System.out.println("CMFileSyncManager::startFileSync() called..");
         CMFileSyncInfo fsInfo = m_cmInfo.getFileSyncInfo();
 
         if(fsInfo.isSyncInProgress()) {
@@ -51,12 +54,41 @@ public class CMFileSyncManager extends CMServiceManager {
             }
         }
 
-        // create a file list in the sync-file-home
-
+        // create a path list in the sync-file-home.
+        List<Path> pathList = createPathList(syncHome);
+        // store the path list in the CMFileSyncInfo.
+        fsInfo.setPathList(pathList);
+        // send the file list to the server
 
         // from here
 
         return true;
+    }
+
+    private List<Path> createPathList(Path syncHome) {
+
+        System.out.println("CMFileSyncManager::createPathList() called..");
+        List<Path> pathList;
+        try {
+            // filter only regular files -> change to absolute path -> sorted -> change to a list
+            pathList = Files.walk(syncHome)
+                    .filter(Files::isRegularFile)
+                    .map(path -> path.toAbsolutePath().normalize())
+                    .sorted()
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        if( pathList.isEmpty() )
+            System.out.println("The sync-home is empty.");
+        else {
+            for(Path p : pathList)
+                System.out.println(p);
+        }
+
+        return pathList;
     }
 
 }
