098fecf93aee832250972599e198af9459301445
Mingyu Lim <mlim@konkuk.ac.kr>
2021-11-04 13:43:24 +0900

    added processSTART_FILE_LIST() to CMFileSyncEventHandler.
    - added a hashtable of file-entry list to CMFileSyncInfo.

--- CM/src/main/java/kr/ac/konkuk/ccslab/cm/event/handler/CMFileSyncEventHandler.java
+++ CM/src/main/java/kr/ac/konkuk/ccslab/cm/event/handler/CMFileSyncEventHandler.java
@@ -1,9 +1,18 @@
 package kr.ac.konkuk.ccslab.cm.event.handler;
 
+import kr.ac.konkuk.ccslab.cm.entity.CMFileSyncEntry;
 import kr.ac.konkuk.ccslab.cm.event.CMEvent;
 import kr.ac.konkuk.ccslab.cm.event.filesync.CMFileSyncEvent;
 import kr.ac.konkuk.ccslab.cm.event.mqttevent.CMMqttEvent;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.manager.CMEventManager;
+import kr.ac.konkuk.ccslab.cm.manager.CMFileSyncManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Vector;
 
 public class CMFileSyncEventHandler extends CMEventHandler {
 
@@ -43,13 +52,67 @@ public class CMFileSyncEventHandler extends CMEventHandler {
 
         return processResult;
     }
-    
+
+    // called at the server
     private boolean processSTART_FILE_LIST(CMFileSyncEvent fse) {
-        // from here
-        return false;
+
+        if(CMInfo._CM_DEBUG) {
+            System.out.println("CMFileSyncEventHandler.processSTART_FILE_LIST() called..");
+            System.out.println("event = "+fse);
+        }
+
+        String userName = fse.getUserName();
+        // get the file-sync manager
+        CMFileSyncManager fsManager = (CMFileSyncManager) m_cmInfo.getServiceManagerHashtable()
+                .get(CMInfo.CM_FILE_SYNC_MANAGER);
+        // get server sync home for userName
+        Path serverSyncHome = fsManager.getServerSyncHome(userName);
+        // check and create the server sync home
+        if(Files.notExists(serverSyncHome)) {
+            try {
+                Files.createDirectories(serverSyncHome);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        // check the file-entry-list hashtable
+        List<CMFileSyncEntry> entryList = m_cmInfo.getFileSyncInfo().getFileEntryListHashtable().get(userName);
+        if( entryList == null ) {
+            // create a new list
+            entryList = new Vector<>();
+            m_cmInfo.getFileSyncInfo().getFileEntryListHashtable().put(userName, entryList);
+        }
+        else {
+            // emtpry the list
+            entryList.clear();
+        }
+
+        // create the ack event
+        CMFileSyncEvent ackFse = new CMFileSyncEvent();
+        ackFse.setID(CMFileSyncEvent.START_FILE_LIST_ACK);
+        String serverName = m_cmInfo.getInteractionInfo().getMyself().getName();
+        ackFse.setSender( serverName );
+        ackFse.setReceiver( userName );
+        ackFse.setUserName( userName );
+        ackFse.setNumTotalFiles( fse.getNumTotalFiles() );
+        ackFse.setReturnCode(1);    // always success
+
+        // send the ack event to the client
+
+        return CMEventManager.unicastEvent(ackFse, userName, m_cmInfo);
     }
 
+    // processed at the client
     private boolean processSTART_FILE_LIST_ACK(CMFileSyncEvent fse) {
+
+        if(CMInfo._CM_DEBUG) {
+            System.out.println("CMFileSyncEventHandler.processSTART_FILE_LIST_ACK() called..");
+            System.out.println("event = "+fse);
+        }
+
+        // from here
         return false;
     }
 
--- CM/src/main/java/kr/ac/konkuk/ccslab/cm/info/CMFileSyncInfo.java
+++ CM/src/main/java/kr/ac/konkuk/ccslab/cm/info/CMFileSyncInfo.java
@@ -1,17 +1,22 @@
 package kr.ac.konkuk.ccslab.cm.info;
 
+import kr.ac.konkuk.ccslab.cm.entity.CMFileSyncEntry;
+
 import java.nio.file.Path;
+import java.util.Hashtable;
 import java.util.List;
 
 public class CMFileSyncInfo {
 
     public static final String SYNC_HOME = "FileSyncHome";
     private boolean syncInProgress;
-    private List<Path> pathList;
+    private List<Path> pathList;        // 4 client
+    private Hashtable<String, List<CMFileSyncEntry>> fileEntryListHashtable;    // 4 server
 
     public CMFileSyncInfo() {
         syncInProgress = false;
         pathList = null;
+        fileEntryListHashtable = new Hashtable<>();
     }
 
     public boolean isSyncInProgress() {
@@ -28,5 +33,13 @@ public class CMFileSyncInfo {
 
     public void setPathList(List<Path> pathList) {
         this.pathList = pathList;
+    }
+
+    public Hashtable<String, List<CMFileSyncEntry>> getFileEntryListHashtable() {
+        return fileEntryListHashtable;
+    }
+
+    public void setFileEntryListHashtable(Hashtable<String, List<CMFileSyncEntry>> fileEntryListHashtable) {
+        this.fileEntryListHashtable = fileEntryListHashtable;
     }
 }
