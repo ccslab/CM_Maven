935d38b33c110594561bbf4d4153688858a08992
Mingyu Lim <mlim@konkuk.ac.kr>
2021-11-29 16:24:36 +0900

    added test classes for the end-block-checksum-related file sync events.

---
+++ CM/src/test/java/kr/ac/konkuk/ccslab/cm/event/filesync/CMFileSyncEventEndFileBlockChecksumAckTest.java
@@ -0,0 +1,36 @@
+package kr.ac.konkuk.ccslab.cm.event.filesync;
+
+import kr.ac.konkuk.ccslab.cm.manager.CMEventManager;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.*;
+
+public class CMFileSyncEventEndFileBlockChecksumAckTest {
+    @Test
+    public void marshallUnmarshall() {
+        System.out.println("CMFileSyncEventEndFileBlockChecksumAckTest.marshallUnmarshall() called..");
+
+        CMFileSyncEventEndFileBlockChecksumAck fse = new CMFileSyncEventEndFileBlockChecksumAck();
+        fse.setFileChecksum(2);
+        fse.setBlockSize(700);
+        fse.setFileEntryIndex(5);
+        fse.setTotalNumBlocks(100);
+        fse.setReturnCode(1);
+
+        ByteBuffer msg = CMEventManager.marshallEvent(fse);
+        System.out.println("fse after marshalling = " + fse);
+        CMFileSyncEventEndFileBlockChecksumAck fse2 =
+                (CMFileSyncEventEndFileBlockChecksumAck) CMEventManager.unmarshallEvent(msg);
+        System.out.println("fse2 after unmarshalling = " + fse2);
+
+        assertNotNull(fse2);
+        assertEquals(fse.getBlockSize(), fse2.getBlockSize());
+        assertEquals(fse.getFileChecksum(), fse2.getFileChecksum());
+        assertEquals(fse.getFileEntryIndex(), fse2.getFileEntryIndex());
+        assertEquals(fse.getTotalNumBlocks(), fse2.getTotalNumBlocks());
+        assertEquals(fse.getReturnCode(), fse2.getReturnCode());
+    }
+
+}
---
+++ CM/src/test/java/kr/ac/konkuk/ccslab/cm/event/filesync/CMFileSyncEventEndFileBlockChecksumTest.java
@@ -0,0 +1,32 @@
+package kr.ac.konkuk.ccslab.cm.event.filesync;
+
+import kr.ac.konkuk.ccslab.cm.manager.CMEventManager;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.*;
+
+public class CMFileSyncEventEndFileBlockChecksumTest {
+    @Test
+    public void marshallUnmarshall() {
+        System.out.println("CMFileSyncEventEndFileBlockChecksumTest.marshallUnmarshall() called..");
+
+        CMFileSyncEventEndFileBlockChecksum fse = new CMFileSyncEventEndFileBlockChecksum();
+        fse.setBlockSize(700);
+        fse.setFileEntryIndex(7);
+        fse.setTotalNumBlocks(11);
+
+        ByteBuffer msg = CMEventManager.marshallEvent(fse);
+        System.out.println("fse after marshalling = " + fse);
+        CMFileSyncEventEndFileBlockChecksum fse2 =
+                (CMFileSyncEventEndFileBlockChecksum) CMEventManager.unmarshallEvent(msg);
+        System.out.println("fse2 after unmarshalling = " + fse2);
+
+        assertNotNull(fse2);
+        assertEquals(fse.getBlockSize(), fse2.getBlockSize());
+        assertEquals(fse.getFileEntryIndex(), fse2.getFileEntryIndex());
+        assertEquals(fse.getTotalNumBlocks(), fse2.getTotalNumBlocks());
+    }
+
+}
