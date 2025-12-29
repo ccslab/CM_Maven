b86e47032647bae0265dcaec754ad7071a158dcf
ccslab <mlim@konkuk.ac.kr>
2025-12-29 10:50:49 +0900

    feat(util): CMUUIDConverter 클래스 추가

---
+++ CM/src/main/java/kr/ac/konkuk/ccslab/cm/util/CMUUIDConverter.java
@@ -0,0 +1,30 @@
+package kr.ac.konkuk.ccslab.cm.util;
+
+import java.util.UUID;
+
+public class CMUUIDConverter {
+    // Converts UUID to String
+    public static String uuidToString(UUID uuid) {
+        // If uuid is null, return empty string
+        if (uuid == null) {
+            return "";
+        }
+        return uuid.toString();
+    }
+
+    // Converts String to UUID
+    public static UUID stringToUuid(String strUuid) {
+        //  If string is null or empty, return null
+        if (strUuid == null || strUuid.isEmpty()) {
+            return null;
+        }
+
+        try {
+            //  Convert string to UUID object
+            return UUID.fromString(strUuid);
+        } catch (IllegalArgumentException e) {
+            System.err.println("CMUUIDConverter.stringToUuid(), Invalid UUID format: " + strUuid);
+            return null;
+        }
+    }
+}
