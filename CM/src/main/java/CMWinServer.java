2883232284eed5a8d796d586f7b51e3fc1e9e297
ccslab <mlim@konkuk.ac.kr>
2026-01-14 20:52:55 +0900

    fix: CMWinServer.printLoginUsers() CMMember 구조 변경에 따른 수정

--- CM/src/main/java/CMWinServer.java
+++ CM/src/main/java/CMWinServer.java
@@ -1691,18 +1691,20 @@ public class CMWinServer extends JFrame {
 		}
 		
 		printMessage("Currently ["+loginUsers.getMemberNum()+"] users are online.\n");
-		Vector<CMUser> loginUserVector = loginUsers.getAllMembers();
-		Iterator<CMUser> iter = loginUserVector.iterator();
+		// The member table is changed from Vector to Hashtable<String, List<CMUser>> to support multiple logins (UUIDs).
+		// The return type of getAllMembers() is updated accordingly.
+		Hashtable<String, List<CMUser>> loginUserTable = loginUsers.getAllMembers();
 		int nPrintCount = 0;
-		while(iter.hasNext())
-		{
-			CMUser user = iter.next();
-			printMessage(user.getName()+" ");
-			nPrintCount++;
-			if((nPrintCount % 10) == 0)
-			{
-				printMessage("\n");
-				nPrintCount = 0;
+		// Iterate through the values (List<CMUser>) of the Hashtable, and then iterate through each CMUser in the list.
+		for(List<CMUser> userList : loginUserTable.values()) {
+			for (CMUser user : userList) {
+				printMessage(user.getName()+" ");
+				nPrintCount++;
+				if((nPrintCount % 10) == 0)
+				{
+					printMessage("\n");
+					nPrintCount = 0;
+				}
 			}
 		}
 	}
